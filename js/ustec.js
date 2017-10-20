e0820fab8fc5463d71e0cc27e5191e0297a35728
Mònica Ramírez Arceda <monica@probeta.net>
2017-10-20 16:53:26 +0200

    View entry body and go back

--- js/ustec.js
+++ js/ustec.js
@@ -13,6 +13,7 @@ var App = new Vue({
   el: '#app',
   data: {
     entries: [], // initialize empty array
+    entry_text: "",
     entries_list_visible: true,
     entry_visible: false
   },
@@ -21,5 +22,22 @@ var App = new Vue({
     $.getJSON(dataURL, function(data) {
       self.entries = data;
     });
+  },
+  methods: {
+    get_entry: function (pk) {
+    	for (i=0; i< this.entries.length; i++){
+        	if(this.entries[i].pk == pk){
+        		this.entry_text = this.entries[i].body;
+        		break;
+        	}
+        }
+    	this.entries_list_visible = false;
+        this.entry_visible = true;
+        
+    },
+    go_home: function () {
+    	this.entries_list_visible = true;
+        this.entry_visible = false;
+    },
   }
 })
