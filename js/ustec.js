419716d8ab6c26439b04339463003e1a7586ddc5
Mònica Ramírez Arceda <monica@probeta.net>
2017-10-20 17:39:47 +0200

    Add comments

--- js/ustec.js
+++ js/ustec.js
@@ -14,13 +14,14 @@ var dataURL = protocol + host + '/json';
 var App = new Vue({
   el: '#app',
   data: {
-    entries: [], // initialize empty array
-    entry_text: "",
-    entries_list_visible: true,
-    entry_visible: false
+    entries: [], // empty array for all entries
+    entry_text: "", // current clicked entry
+    entries_list_visible: true, // flag that indicates if we are in main page
+    entry_visible: false // flag that indicates if we are viewing an entry
   },
   mounted() { // when the Vue app is booted up, this is run automatically.
     var self = this // create a closure to access component in the callback below
+    // Get all entries from server
     $.getJSON(dataURL, function(data) {
       self.entries = data;
       // Add host to images URL
@@ -31,21 +32,28 @@ var App = new Vue({
     });
   },
   methods: {
+	// Get clicked entry
     get_entry: function (pk) {
+    	// Search clicked entry
     	for (i=0; i< this.entries.length; i++){
-        	if(this.entries[i].pk == pk){
-        		if (this.entries[i].body != '')
+        	if(this.entries[i].pk == pk){        		
+        		if (this.entries[i].body != ''){
+        			// If entry has body, show summary as title and body
         			this.entry_text = "<h2>" + this.entries[i].summary + "</h2>" + this.entries[i].body;
-        		else
+        		} else {
+        			// If entry has no body, show only summary
         			this.entry_text = this.entries[i].summary;
+        		}
         		break;
         	}
         }
+    	// Hide list of entries and show clicked entry  
     	this.entries_list_visible = false;
-        this.entry_visible = true;
-        
+        this.entry_visible = true;        
     },
+    // Go to main page
     go_home: function () {
+    	// Hide clicked entry and show list of entries
     	this.entries_list_visible = true;
         this.entry_visible = false;
     },
